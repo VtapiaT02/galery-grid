9625c4f745a90b3d5f8bde3ca74d53167804c867
Victor Tapia <vtapia02@hotmail.com>
2024-06-06 13:00:53 -0600

    Proyecto Final

---
+++ js/main.js
@@ -0,0 +1,15 @@
+const photoContainer = document.getElementById("photo-container");
+
+let content = "";
+
+for (let i=0; i<50; i++) {
+    content += getPhotoElement(i+1);
+}
+
+photoContainer.innerHTML = content
+
+function getPhotoElement(photo_id) {
+    return `<a href="#" class="photo">
+    <img alt="Photo ${photo_id}" src="./img/photo_${photo_id}.jpg"/>
+    </a>`;
+}
